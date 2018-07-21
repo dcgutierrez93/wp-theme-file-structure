9627016f938f9ce4da8c80a08121ff2652b43e81
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-07-21 11:30:45 -0500

    Added Body and Css IMG and Js

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
 function myFunction_files() {
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    // Load CDN Font Awesome Icons
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     // Links your custom stylesheet
     wp_enqueue_style('my_main_styles', get_stylesheet_uri());
 }
